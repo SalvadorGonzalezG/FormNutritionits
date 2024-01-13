2700ca8fe364d00917406b64dbfe5564b69c2e3c
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-01-13 15:02:25 -0600

    add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
@@ -9,6 +9,22 @@ function App() {
   const [paciente, setPaciente] = useState([]);
   // cada uno de los pacientes es un objeto por lo tanto el estado comienza con un objeto vacio
   const [upPaciente, setUpPaciente] = useState({})
+
+  //Orden en el que se definen los useEfect es en elque se van ejecutando.
+  useEffect(()=>{
+    // obtener lo que haya en localStorage
+    const obtenerLocalStorage = () =>{ 
+                                  // si no hay nada el lS agrega un arreglo vacio.
+      const pacienteLocalStorage = JSON.parse(localStorage.getItem('paciente')) ?? [];
+      setPaciente(pacienteLocalStorage)
+    }
+    obtenerLocalStorage();
+    // dependencia vacia para que se ejecute una unica vez.
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('paciente', JSON.stringify( paciente ))
+  },[paciente])
 
   // funcion para eliminar un paciente
   const deletePaciente = (id) => {
